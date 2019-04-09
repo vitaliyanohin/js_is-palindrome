4d89ae77a338f8bfc5aff40cccceca1b4ce9e3ed
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-09 14:27:41 +0300

    added palindrome description

--- src/isPalindrome.js
+++ src/isPalindrome.js
@@ -3,6 +3,9 @@
 /**
  * Implement isPalindrome function:
  *
+ * A palindrome is a word, phrase, or other sequence of characters which reads
+ * the same backward as forward, such as 'madam' or 'racecar'
+ *
  * Given a string, determine if it is a palindrome, considering only
  * alphanumeric characters and ignoring cases. Empty string is a valid
  * palindrome.
