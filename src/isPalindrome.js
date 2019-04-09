5f84887421b477797efcd458e4d229302f8f1c37
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-09 12:20:17 +0300

    implemented task and tests

---
+++ src/isPalindrome.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * Implement isPalindrome function:
+ *
+ * Given a string, determine if it is a palindrome, considering only
+ * alphanumeric characters and ignoring cases. Empty string is a valid
+ * palindrome.
+ *
+ *
+ * isPalindrome('A man, a plan, a canal: Panama') === true
+ * isPalindrome('race a car') === false
+ *
+ * @param {string} str
+ *
+ * @return {boolean}
+ */
+function isPalindrome(str) {
+  // write code here
+}
+
+module.exports = isPalindrome;
